decc71a4e1192a5cea210596352066ac99f9b2f0
Abdil Kaya <c@linux.com>
2016-02-29 13:14:41 +0100

    QRDecoder optimalisation : RamDisk usage; including mkRamdisk.sh

--- QRDecoder.sh
+++ QRDecoder.sh
@@ -15,39 +15,37 @@ errorTest=false
 rm -rf /mnt/ramdisk/*
 
 function ErrorCheck {
+	grepOut="$(tail /mnt/ramdisk/raspistill.dat)";
+	grep0Out=$(sed -n 1p /mnt/ramdisk/QR.dat);
+	grep2Out=$(sed -n 2p /mnt/ramdisk/QR.dat);
 
+	#test grepOut for Camera Error
+	$errorTest&& grepOut="mmal"
 
-        grepOut="$(tail /mnt/ramdisk/QR.dat)";
-        grep0Out=$(sed -n 1p /mnt/ramdisk/QR.dat);
-        grep2Out=$(sed -n 2p /mnt/ramdisk/QR.dat);
-
-        #test grepOut for Camera Error
-        $errorTest&& grepOut="mmal"
-
-        if echo "$grepOut" | grep -q mmal;then
-                error=true
+	if echo "$grepOut" | grep -q mmal;then
+		error=true
 		sed -i '3i\1' /mnt/ramdisk/QR.dat                
-                $debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} ERROR CODE 1: Camera Error : [${RED}NOK${NC}]"
-        elif ! echo "$grep2Out" | grep -q lading;then
-                error=true
+		$debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} ERROR CODE 1: Camera Error : [${RED}NOK${NC}]"
+	elif ! echo "$grep2Out" | grep -q lading;then
+		error=true
 		sed -i '3i\2' /mnt/ramdisk/QR.dat                
-                $debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} ERROR CODE 2: No QR data Error : [${RED}NOK${NC}]"
-        elif [[ "${#grep0Out}" -lt "2" ]];then
-                error=true
+		$debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} ERROR CODE 2: No QR data Error : [${RED}NOK${NC}]"
+	elif [[ "${#grep0Out}" -lt "2" ]];then
+		error=true
 		sed -i '3i\3' /mnt/ramdisk/QR.dat                
-                $debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} ERROR CODE 3: Other Error : [${RED}NOK${NC}]"
-        else
+		$debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} ERROR CODE 3: Other Error : [${RED}NOK${NC}]"
+	else
 		sed -i '3i\0' /mnt/ramdisk/QR.dat
-        fi
-        printOk
+	fi
+	printOk
 }
 
 function printOk {
-        if [[ "$error" == "false" ]];then
-                $debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} Raspistill saved ${ORANGE}QR.jpg${NC} to ${CYAN}tmpfs: /mnt/ramdisk/ (RAM)${NC} : [${GREEN}OK${NC}]"
-                $debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} QR-code data written on line 2  of ${ORANGE}/mnt/ramdisk/QR.dat${NC} : [${GREEN}OK${NC}]"
-                $debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} QR-code data read. Time check: ${ORANGE}$timeCheck${NC}, written on line 1 of ${ORANGE}/mnt/ramdisk/QR.dat${NC} : [${GREEN}OK${NC}]"
-        fi
+	if [[ "$error" == "false" ]];then
+		$debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} Raspistill saved ${ORANGE}QR.jpg${NC} to ${CYAN}tmpfs: /mnt/ramdisk/ (RAM)${NC} : [${GREEN}OK${NC}]"
+		$debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} QR-code data written on line 2  of ${ORANGE}/mnt/ramdisk/QR.dat${NC} : [${GREEN}OK${NC}]"
+		$debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} QR-code data read. Time check: ${ORANGE}$timeCheck${NC}, written on line 1 of ${ORANGE}/mnt/ramdisk/QR.dat${NC} : [${GREEN}OK${NC}]"
+	fi
 }
 
 
@@ -62,7 +60,7 @@ echo "0" > /mnt/ramdisk/QR.dat
 $debug&& echo -e "${BG}[$(date | cut -c 12-19)]${NC} Raspistill saving ${ORANGE}QR.jpg${NC} to ${CYAN}tmpfs: /mnt/ramdisk/ (RAM)${NC}"
 #Raspistill: vertical and horizontal flip: (-vf, -hf) in output (-o) file (QR.jpg), saved on RAM, accessible at /io 
 #-w -h : width and height; lowest 640x480; should hopefully be faster at lower quality shots. -q 10: quality (0-100). 100: uncompressed. 10 should deliver lower quality.
-raspistill -o /mnt/ramdisk/QR.jpg &
+raspistill -o /mnt/ramdisk/QR.jpg >> /mnt/raspistill.dat
 wait
 
 #print QR-code data (-q parameter suppresses other output) 
