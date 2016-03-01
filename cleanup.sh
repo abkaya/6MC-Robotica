1d993fb5af6706b60a9819137f7278c85c66473a
Robin Janssens <robin@robinjanssens.net>
2016-03-01 19:01:06 +0100

    added cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#========================================
+#
+#  Script to remove remaining data
+#  Written by Abdil Kaya & Robin Janssens
+#
+#========================================
+
+rm QRDecoder.sh
+rm tagreader.sh
+rm mkRamdisk.sh
+rm cleanup.sh
