2857eb01b404098662401be2a0d4ea38e7b4d4a3
Abdil Kaya <c@linux.com>
2016-03-01 12:06:03 +0100

    Addition of logic to check whether or not ramdisk at /mnt/ramdisk already exists or not

--- test.sh
+++ test.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
-b=$(sed -n 2p QR.dat);
-if ! echo $b | grep -q ldadin; then
-echo "ok"
+
+#=======================================================
+#
+#  QRDecoder.sh |  
+#  Written by Abdil Kaya & Robin Janssens
+#
+#=======================================================
+
+CreateDisk() {
+	sudo mkdir /mnt/ramdisk/
+	sudo mount -t tmpfs -o size=100m tmpfs /mnt/ramdisk/
+	sudo chown pi /mnt/ramdisk/
+}
+
+dirMade=$(ls -laG / | grep /mnt/ramdisk)
+diskMade=$(df -h | grep /mnt/ramdisk)
+if [[ ! $dirMade && ! $diskMade ]]; then
+	CreateDisk
+	echo "Ramdisk not found or not mounted on /mnt/ramdisk/; creating now"
 else
-echo "nokbitch"
+	echo "Ramdisk found and mounted on /mnt/ramdisk"
 fi
+
+#to unallocate this space; mke2fs -m 0 /mnt/ramdisk
+
+##TO DO:
+#add the following line to /etc/fstab in order for the ramdisk to exist upon reboot:
+#tmpfs           /mnt/ramdisk/       tmpfs   nodev,nosuid,noexec,nodiratime,size=100m        0           0
+
+##INFO:
+#df -h output:
+#Filesystem      Size  Used Avail Use% Mounted on
+#/dev/sda1        20G  8.6G  9.8G  47% /
+#udev            2.0G   12K  2.0G   1% /dev
+#tmpfs           808M  904K  808M   1% /run
+#none            5.0M     0  5.0M   0% /run/lock
+#none            2.0G  172K  2.0G   1% /run/shm
+#none            100M   16K  100M   1% /run/user
+#tmpfs           100M     0  100M   0% /mnt/ramdisk
+
